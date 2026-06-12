0e75406065de6802bb6e4926f25590e8554ee6bf
Md Fardin Rahman <mdfardinrahman44@gmail.com>
2026-06-12 11:20:02 +0600

    First Print with java

---
+++ console-print/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    public static void main(String[] args){
+        System.out.print("2 + 3 = ");
+        System.out.println(2+3);
+
+        System.out.println("5 + 5 = " + (5+5));
+        System.out.println("5 - 5 = " + (5-5));
+        System.out.println("5 * 5 = " + (5*5));
+        System.out.println("5 / 5 =" + (5/5));
+    }
+}
---
+++ console-print/Print5times.java
@@ -0,0 +1,9 @@
+public class Print5times{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
